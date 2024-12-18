f8955b4f264f3c845b8a8826d8fc27e8d469cb81
woong99 <dndry1072@naver.com>
2024-12-18 12:27:09 +0900

    feat: multi-external-api project init

---
+++ multi-external-api/src/main/kotlin/potatowoong/multiexternalapi/MultiExternalApiApplication.kt
@@ -0,0 +1,11 @@
+package potatowoong.multiexternalapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiExternalApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MultiExternalApiApplication>(*args)
+}
---
+++ multi-external-api/src/test/kotlin/potatowoong/multiexternalapi/MultiExternalApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package potatowoong.multiexternalapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiExternalApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
