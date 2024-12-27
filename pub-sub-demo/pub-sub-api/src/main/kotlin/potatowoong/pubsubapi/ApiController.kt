154430c4f92f53b498bd8e5e9000e2189d204787
woong99 <dndry1072@naver.com>
2024-12-27 14:54:32 +0900

    [pub-sup-api] feat: 티켓 후처리 API 추가

--- pub-sub-demo/pub-sub-api/src/main/kotlin/potatowoong/pubsubapi/ApiController.kt
+++ pub-sub-demo/pub-sub-api/src/main/kotlin/potatowoong/pubsubapi/ApiController.kt
@@ -5,14 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
+import kotlin.random.Random
 
 @RestController
 @RequestMapping("/api")
 class ApiController {
 
     @GetMapping
-    fun api():ResponseEntity<String> {
+    fun api(): ResponseEntity<String> {
         Thread.sleep(2000)
+
         return ResponseEntity.ok(UUID.randomUUID().toString().replace("-", ""))
+    }
+
+    @GetMapping("/ticket")
+    fun ticketProcess(): ResponseEntity<String> {
+        Thread.sleep(1500)
+
+        // 테스트 상황을 구현하기 위해 랜덤하게 예외를 발생
+        if (Random.nextInt(1, 4) == 3) {
+            return ResponseEntity.ok("FAIL")
+        }
+
+        return ResponseEntity.ok("OK")
     }
 }
